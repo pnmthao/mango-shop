eb41ec293dca13b0f1273b1d6d9b8ac4e14b8e5e
Tinh Dao <dcongtinh@gmail.com>
2020-04-16 08:49:10 +0700

    Update .htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,10 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if ($this->app->environment() == 'production') {
-        //     URL::forceScheme('https');
-        // }
-        URL::forceScheme('https'); //Production
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
+        // URL::forceScheme('https'); //Production
         view()->composer('../header', function ($view) {
             $loai_sp = ProductType::all();
             $view->with('loai_sp', $loai_sp);
