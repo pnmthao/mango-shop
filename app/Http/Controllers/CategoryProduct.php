a8b14558e701eca7b5d92c9b9fb2bc582d415933
Tinh Dao <dcongtinh@gmail.com>
2020-04-16 19:44:44 +0700

    Remove public/ path

--- app/Http/Controllers/CategoryProduct.php
+++ app/Http/Controllers/CategoryProduct.php
@@ -6,40 +6,47 @@ use Illuminate\Http\Request;
 use DB;
 use Session;
 use Redirect;
+
 Session_start();
 class CategoryProduct extends Controller
 {
-    public function AuthLogin(){
+    public function AuthLogin()
+    {
         $admin_id = Session::get('admin_id');
-        if($admin_id){
+        if ($admin_id) {
             return Redirect('dashboard');
-        }else{
+        } else {
             return Redirect('admin')->send();
         }
     }
-    public function add_category_product(){
+    public function add_category_product()
+    {
         $this->AuthLogin();
         return view('admin.add_category_product');
     }
-    public function edit_category_product($category_product_id){
+    public function edit_category_product($category_product_id)
+    {
         $this->AuthLogin();
-        $edit_category_product = DB::table('type_products')->where('id',$category_product_id)->get();
-        $manager_category_product = view('admin.edit_category_product')->with('edit_category_product',$edit_category_product);
-        return view('admin_layout')->with('admin.edit_category_product',$manager_category_product);
+        $edit_category_product = DB::table('type_products')->where('id', $category_product_id)->get();
+        $manager_category_product = view('admin.edit_category_product')->with('edit_category_product', $edit_category_product);
+        return view('admin_layout')->with('admin.edit_category_product', $manager_category_product);
     }
-    public function delete_category_product($category_product_id){
+    public function delete_category_product($category_product_id)
+    {
         $this->AuthLogin();
-        DB::table('type_products')->where('id',$category_product_id)->delete($category_product_id);
-        Session::put('message','Xóa danh mục sản phẩm thành công');
+        DB::table('type_products')->where('id', $category_product_id)->delete($category_product_id);
+        Session::put('message', 'Xóa danh mục sản phẩm thành công');
         return redirect('all-category-product');
     }
-    public function all_category_product(){
+    public function all_category_product()
+    {
         $this->AuthLogin();
         $all_category_product = DB::table('type_products')->get();
-        $manager_category_product = view('admin.all_category_product')->with('all_category_product',$all_category_product);
-        return view('admin_layout')->with('admin.all_category_product',$manager_category_product);
+        $manager_category_product = view('admin.all_category_product')->with('all_category_product', $all_category_product);
+        return view('admin_layout')->with('admin.all_category_product', $manager_category_product);
     }
-    public function save_category_product(Request $req){
+    public function save_category_product(Request $req)
+    {
         $this->AuthLogin();
         $data = array();
         $data['name'] = $req->category_product_name;
@@ -51,18 +58,19 @@ class CategoryProduct extends Controller
         $data['image'] = $req->category_product_image;
         $data['image'] = '';
         $get_image = $req->file('category_product_image');
-        if($get_image){
+        if ($get_image) {
             $get_name_image = $get_image->getClientOriginalName();
-            $name_image = current(explode('.',$get_name_image));
-            $new_image = $name_image.rand(0,99).'.'.$get_image->getClientOriginalExtension();
-            $get_image->move('public/uploads/category_product',$new_image);
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image . rand(0, 99) . '.' . $get_image->getClientOriginalExtension();
+            $get_image->move('uploads/category_product', $new_image);
             $data['image'] = $new_image;
-        }        
+        }
         DB::table('type_products')->insert($data);
-        Session::put('message','Thêm danh mục sản phẩm thành công');
+        Session::put('message', 'Thêm danh mục sản phẩm thành công');
         return redirect('all-category-product');
     }
-    public function update_category_product(Request $req, $category_product_id){
+    public function update_category_product(Request $req, $category_product_id)
+    {
         $this->AuthLogin();
         $data = array();
         $data['name'] = $req->category_product_name;
@@ -71,30 +79,32 @@ class CategoryProduct extends Controller
         $data['description_en'] = $req->category_product_description_en;
         $data['created_at'] = $data['updated_at'] = date('Y-m-d H:i:s');
         $get_image = $req->file('category_product_image');
-        if($get_image){
+        if ($get_image) {
             $get_name_image = $get_image->getClientOriginalName();
-            $name_image = current(explode('.',$get_name_image));
-            $new_image = $name_image.rand(0,99).'.'.$get_image->getClientOriginalExtension();
-            $get_image->move('public/uploads/category_product',$new_image);
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image . rand(0, 99) . '.' . $get_image->getClientOriginalExtension();
+            $get_image->move('uploads/category_product', $new_image);
             $data['image'] = $new_image;
-            DB::table('type_products')->where('id',$category_product_id)->update($data);
-            Session::put('message','Cập nhật danh mục sản phẩm thành công');
+            DB::table('type_products')->where('id', $category_product_id)->update($data);
+            Session::put('message', 'Cập nhật danh mục sản phẩm thành công');
             return redirect('all-category-product');
-        }     
-        DB::table('type_products')->where('id',$category_product_id)->update($data);
-        Session::put('message','Cập nhật danh mục sản phẩm thành công');
+        }
+        DB::table('type_products')->where('id', $category_product_id)->update($data);
+        Session::put('message', 'Cập nhật danh mục sản phẩm thành công');
         return redirect('all-category-product');
     }
-    public function unactive_category_product($category_product_id){
+    public function unactive_category_product($category_product_id)
+    {
         $this->AuthLogin();
-        DB::table('type_products')->where('id',$category_product_id)->update(['status'=>1]);
-        Session::put('message','Không kích hoạt danh mục sản phẩm');
+        DB::table('type_products')->where('id', $category_product_id)->update(['status' => 1]);
+        Session::put('message', 'Không kích hoạt danh mục sản phẩm');
         return redirect()->back();
     }
-    public function active_category_product($category_product_id){
+    public function active_category_product($category_product_id)
+    {
         $this->AuthLogin();
-        DB::table('type_products')->where('id',$category_product_id)->update(['status'=>0]);
-        Session::put('message','Kích hoạt danh mục sản phẩm thành công');
+        DB::table('type_products')->where('id', $category_product_id)->update(['status' => 0]);
+        Session::put('message', 'Kích hoạt danh mục sản phẩm thành công');
         return redirect()->back();
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,27 +1,33 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use DB;
 use Session;
 use Redirect;
+
 Session_start();
 class UserController extends Controller
 {
-    public function AuthLogin(){
+    public function AuthLogin()
+    {
         $admin_id = Session::get('admin_id');
-        if($admin_id){
+        if ($admin_id) {
             return Redirect('dashboard');
-        }else{
+        } else {
             return Redirect('admin')->send();
         }
     }
-    public function edit_profile($profile_id){
+    public function edit_profile($profile_id)
+    {
         $this->AuthLogin();
-        $edit_profile = DB::table('admin')->where('admin_id',$profile_id)->get();
-        $manager_profile = view('admin.edit_profile')->with('edit_profile',$edit_profile);
-        return view('admin_layout')->with('admin.edit_profile',$manager_profile);
+        $edit_profile = DB::table('admin')->where('admin_id', $profile_id)->get();
+        $manager_profile = view('admin.edit_profile')->with('edit_profile', $edit_profile);
+        return view('admin_layout')->with('admin.edit_profile', $manager_profile);
     }
-    public function update_profile(Request $req, $profile_id){
+    public function update_profile(Request $req, $profile_id)
+    {
         $this->AuthLogin();
         $data = array();
         $data['admin_name'] = $req->profile_name;
@@ -30,18 +36,18 @@ class UserController extends Controller
         $data['admin_password'] = md5($req->profile_password);
         $data['created_at'] = $data['updated_at'] = date('Y-m-d H:i:s');
         $get_image = $req->file('profile_image');
-        if($get_image){
+        if ($get_image) {
             $get_name_image = $get_image->getClientOriginalName();
-            $name_image = current(explode('.',$get_name_image));
-            $new_image = $name_image.rand(0,99).'.'.$get_image->getClientOriginalExtension();
-            $get_image->move('public/uploads/profile',$new_image);
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image . rand(0, 99) . '.' . $get_image->getClientOriginalExtension();
+            $get_image->move('uploads/profile', $new_image);
             $data['admin_image'] = $new_image;
-            DB::table('admin')->where('admin_id',$profile_id)->update($data);
-            Session::put('message','Cập nhật hồ sơ cá nhân thành công');
+            DB::table('admin')->where('admin_id', $profile_id)->update($data);
+            Session::put('message', 'Cập nhật hồ sơ cá nhân thành công');
             return redirect()->back();
-        }     
-        DB::table('admin')->where('admin_id',$profile_id)->update($data);
-        Session::put('message','Cập nhật hồ sơ cá nhân thành công');
+        }
+        DB::table('admin')->where('admin_id', $profile_id)->update($data);
+        Session::put('message', 'Cập nhật hồ sơ cá nhân thành công');
         return redirect()->back();
     }
 }
